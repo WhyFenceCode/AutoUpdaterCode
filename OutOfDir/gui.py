22a84db39d815f7a5b2e85065d0b9cb613908bb2
WhyFenceCode <120431739+WhyFenceCode@users.noreply.github.com>
2024-02-01 15:58:42 -0800

    finnished guis

--- OutOfDir/gui.py
+++ OutOfDir/gui.py
@@ -29,7 +29,7 @@ class install_window(QtWidgets.QDialog):
             color:gray;
             font-size: 20px;
             font-weight: light;
-            background:rgb(56, 48, 76);
+            background:rgb(48, 50, 76);
             margin-left: 10px;
             margin-right: 10px;
             margin-bottom: 15px;
@@ -60,7 +60,7 @@ class install_window(QtWidgets.QDialog):
 
         self.round_widget.setStyleSheet(
             """
-            background:rgb(32, 26, 48);
+            background:rgb(26, 28, 48);
             border-radius: 32px;
             """
         )
@@ -92,7 +92,7 @@ class install_window(QtWidgets.QDialog):
         )
 
         install_subtext = QtWidgets.QLabel(self)
-        install_subtext.setText("Select your options and continue.")
+        install_subtext.setText("PROGRAM WILL RUN AFTER")
         wdg_layout.addWidget(install_subtext)
         install_subtext.setFont(QFont('Calibri')) 
         install_subtext.setStyleSheet(
@@ -101,11 +101,11 @@ class install_window(QtWidgets.QDialog):
             font-size: 20px;
             font-weight: light;
             margin-left: 20px;
-            margin-bottom: 40px;
+            margin-bottom: 90px;
             """
         )
 
-        shortcut_on = QtWidgets.QPushButton("Create Desktop Shortcut")
+        shortcut_on = QtWidgets.QPushButton("CREATE DESKTOP SHORTCUT")
         wdg_layout.addWidget(shortcut_on)
         self.button_group.addButton(shortcut_on, id=1)
         shortcut_on.setFont(QFont('Calibri')) 
@@ -114,7 +114,7 @@ class install_window(QtWidgets.QDialog):
             color:gray;
             font-size: 20px;
             font-weight: light;
-            background:rgb(56, 48, 76);
+            background:rgb(48, 50, 76);
             margin-left: 10px;
             margin-right: 10px;
             margin-bottom: 15px;
@@ -127,7 +127,7 @@ class install_window(QtWidgets.QDialog):
             """
         )
 
-        shortcut_off = QtWidgets.QPushButton("No Desktop Shortcut")
+        shortcut_off = QtWidgets.QPushButton("NO DESKTOP SHORTCUT")
         wdg_layout.addWidget(shortcut_off)
         self.button_group.addButton(shortcut_off, id=2)
         shortcut_off.setFont(QFont('Calibri')) 
@@ -148,8 +148,140 @@ class install_window(QtWidgets.QDialog):
             """
         )
 
+        go = QtWidgets.QPushButton("INSTALL")
+        wdg_layout.addWidget(go)
+        go.setFont(QFont('Calibri')) 
+        go.setStyleSheet(
+            """
+            color:rgb(26, 28, 48);
+            background:rgb(13, 245, 216);
+            font-size: 20px;
+            font-weight: light;
+            margin-left: 35px;
+            margin-right: 35px;
+            margin-bottom: 15px;
+            margin-top: 80px;
+            border-radius: 16px;
+            padding-top: 20px;
+            padding-bottom: 20px;
+            text-align: center;
+            """
+        )
+
+        self.show()
+
+
+
+
+
+class updates_window(QtWidgets.QDialog):
+
+    def __init__(self, version):
+        super().__init__()
+
+        w = 405
+        h = 720
+
+        # Set dialog size
+        self.resize(w, h)
+        # Remove frame
+        self.setWindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.Dialog)
+        # Make the dialog transparent
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+
+        # Round widget
+        self.round_widget = QtWidgets.QWidget(self)
+        self.round_widget.resize(w, h)
+
+        self.round_widget.setStyleSheet(
+            """
+            background:rgb(26, 28, 48);
+            border-radius: 32px;
+            """
+        )
+
+        # Layout setup (if needed)
+        layout = QtWidgets.QVBoxLayout(self)
+        layout.addWidget(self.round_widget)
+        
+
+        wdg_layout = QtWidgets.QVBoxLayout()
+        wdg_layout.setAlignment(QtCore.Qt.AlignTop)
+        self.round_widget.setLayout(wdg_layout)
+
+        update_text = QtWidgets.QLabel(self)
+        update_text.setText("UPDATE")
+        wdg_layout.addWidget(update_text)
+        update_text.setFont(QFont('Calibri')) 
+        update_text.setStyleSheet(
+            """
+            color:white;
+            font-size: 36px;
+            font-weight: normal;
+            margin-left: 20px;
+            margin-top: 40px;
+            """
+        )
+
+        install_subtext = QtWidgets.QLabel(self)
+        install_subtext.setText("PROGRAM WILL RUN AFTER")
+        wdg_layout.addWidget(install_subtext)
+        install_subtext.setFont(QFont('Calibri')) 
+        install_subtext.setStyleSheet(
+            """
+            color:gray;
+            font-size: 20px;
+            font-weight: light;
+            margin-left: 20px;
+            margin-bottom: 90px;
+            """
+        )
+
+        version_text = QtWidgets.QLabel(self)
+        version_text.setText(str(version))
+        wdg_layout.addWidget(version_text)
+        version_text.setFont(QFont('Calibri')) 
+        version_text.setStyleSheet(
+            """
+            color:gray;
+            font-size: 20px;
+            font-weight: light;
+            background:rgb(48, 50, 76);
+            margin-left: 10px;
+            margin-right: 10px;
+            margin-bottom: 50px;
+            margin-top: 50px;
+            border-radius: 16px;
+            padding-top: 20px;
+            padding-bottom: 20px;
+            padding-left: 10px;
+            text-align: left;
+            """
+        )
+
+        go = QtWidgets.QPushButton("UPDATE")
+        wdg_layout.addWidget(go)
+        go.setFont(QFont('Calibri')) 
+        go.setStyleSheet(
+            """
+            color:rgb(26, 28, 48);
+            background:rgb(13, 245, 216);
+            font-size: 20px;
+            font-weight: light;
+            margin-left: 35px;
+            margin-right: 35px;
+            margin-bottom: 15px;
+            margin-top: 80px;
+            border-radius: 16px;
+            padding-top: 20px;
+            padding-bottom: 20px;
+            text-align: center;
+            """
+        )
+
         self.show()
 
 app = QtWidgets.QApplication([])
 win = install_window()
+#win = updates_window("1.0.0")
 app.exec()
